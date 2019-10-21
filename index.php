9494e02b9ea1b7a8ebf0215430a102e9f25cefd7
dungnm <dungnm@zotabox.com>
2019-10-21 17:43:44 +0700

    ad

--- index.php
+++ index.php
@@ -314,8 +314,9 @@
 								<div class="title-block">hành giả hỏi đạo</div>
 								<a href="#" class="paddle-left">Xem thêm >></a>
 							</div>
-							<div class="news-item">
+							<div class="news-item col-xs-12">
 									<div class="title-news">
+										<div class="double-padles">>></div>
 										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
 									</div>
 
@@ -323,17 +324,19 @@
 										Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 									</div>
 								</div>
-								<div class="news-item">
+								<div class="news-item col-xs-12">
 									<div class="title-news">
+										<div class="double-padles">>></div>
 										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
 									</div>
 
-									<div class="content-news">
+									<div class="content-news ">
 										Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 									</div>
 								</div>
-								<div class="news-item">
+								<div class="news-item col-xs-12">
 									<div class="title-news">
+										<div class="double-padles">>></div>
 										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
 									</div>
 
@@ -351,6 +354,79 @@
 					</div>
 				</div>
 			</div>
+			<footer>
+				<div class="row">
+					<div class="menu-footer col-lg-12">
+						<ul class="nav nav-tabs">
+							<li class="nav-item col-lg-1">
+								<a class="nav-link " href="#">Trang chủ</a>
+							</li>
+							<li class="nav-item col-lg-2">
+								<a class="nav-link active" href="#">Sách gối đầu tu tập</a>
+							</li>
+							<li class="nav-item dropdown col-lg-2">
+								<a class="nav-link" href="#">Pháp thực hành</a>
+
+							</li>
+							<li class="nav-item col-lg-2">
+								<a class="nav-link" href="#">Các bài giảng pháp</a>
+							</li>
+							<li class="nav-item col-lg-2">
+								<a class="nav-link" href="#">Thầy thanh thiện</a>
+							</li>
+							<li class="nav-item col-lg-2">
+								<a class="nav-link" href="#">Hành giả hồi đạo</a>
+							</li>
+							<li class="nav-item col-lg-1">
+								<a class="nav-link" href="#">Video/ audio</a>
+							</li>
+						</ul>
+					</div>
+					<div class="footer-content col-xs-12">
+						<div class="logo-wrapper col-xs-5">
+							<div class="logo-content col-xs-6">
+								<div class="logo-image-wraper">
+									<div class="logo-image">
+										<img src="<?php echo get_stylesheet_directory_uri() ?>/images/phathien.png" alt="">
+									</div>
+								</div>
+							</div>
+							<div class="logo-title col-xs-6">
+								<div class="title-top">tu hành đúng</div>
+								<div class="title-bottom">chánh phật pháp</div>
+
+							</div>
+						</div>
+						<div class="link-wrapper col-xs-3">
+							<div class="list-news-link col-xs-6">
+								<div class="link-item"><a href="">Link bài viết 1</a></div>
+								<div class="link-item"><a href="">Link bài viết 2</a></div>
+								<div class="link-item"><a href="">Link bài viết 3</a></div>
+								<div class="link-item"><a href="">Link bài viết 4</a></div>
+							</div>
+							<div class="list-news-link col-xs-6">
+								<div class="link-item"><a href="">Link bài viết 1</a></div>
+								<div class="link-item"><a href="">Link bài viết 2</a></div>
+								<div class="link-item"><a href="">Link bài viết 3</a></div>
+								<div class="link-item"><a href="">Link bài viết 4</a></div>
+							</div>
+							</div>
+						<div class="contact-wrapper col-xs-4">
+							<div class="title-contact">
+								Địa chỉ liên hệ
+							</div>
+							<div class="contact-content">
+								<p>101 Tây Sơn, Quận Đống Đa, Hà Nội</p>
+								<p>Email: tuhanhdungchanhphatphap@gmail.com</p>
+								<p>ĐT: (012) 123 456/Fax: (034) 123 456</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+
+				
+			</footer>
 
 
 		</div> <!-- /.container -->
